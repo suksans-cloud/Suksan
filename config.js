bd96857a22f4b720f40f1b068992d7a3d2ccd914
suksans-cloud <suksans@gmail.com>
2026-09-19 16:16:10 +0700

    Add files via upload

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
  * แก้ URL ตรงนี้ที่เดียวพอ (หลัง deploy Apps Script แล้วคัดลอก URL ที่ลงท้าย /exec)
  * ทุกหน้าที่ต้องคุยกับ Apps Script (เข้าสู่ระบบ, การตั้งค่า, รายงานสรุปรายเดือน) จะอ่านค่านี้
  */
-window.MFF_ENDPOINT = 'https://script.google.com/macros/s/AKfycbyHSUQbkUn6w_ZRQn-RpElvoZIdrer3gyO_RP7HRwrjfgTovCob1CxMbhGZPq_krSCYiA/exec';
+window.MFF_ENDPOINT = 'https://script.google.com/macros/s/AKfycbynOD-lsfFIRi5B2iqNKwBYVbzDi3jREoSlYaiWYctDawj5Qhx-oL8jFMJXNKtTO2UbPg/exec';
